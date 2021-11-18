7e6d53087df6b9c37bc07078bc661a4904fe2478
BIGBEASTISHANK <77890275+BIGBEASTISHANK@users.noreply.github.com>
2021-11-18 23:28:49 +0530

    Add files via upload

---
+++ config/bspwm/startup.sh
@@ -0,0 +1,22 @@
+## Run Keybindings
+sxhkd &
+
+## Restore Background
+feh --bg-scale ~/Pictures/Wallpapers/cyberpunk.jpg
+
+## Polybar
+$HOME/.config/polybar/launch.sh
+
+## Compositor
+picom &
+
+## ScreenShot
+flameshot &
+
+## Sound Management
+amixer set Master on &
+amixer set Master 100 &
+amixer set Headphone on &
+amixer set Headphone 100 &
+amixer set Speaker 100 &
+amixer set Speaker off &
---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+if status --is-login
+    neofetch | lolcat
+    echo Welcome BIGBEASTISHANK, Give Me Command To Run! | lolcat
+end
+
+function on_exit --on-event fish_exit
+    echo Exiting
+end
